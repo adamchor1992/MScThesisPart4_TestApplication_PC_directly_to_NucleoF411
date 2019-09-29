efa953462ba34700453734136c54980d1c969244
Adam Choragwicki <adamchor1992@gmail.com>
2019-09-29 21:41:45 +0200

    updated executable files

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[17];
-    char stringdata0[583];
+    QByteArrayData data[18];
+    char stringdata0[611];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -48,7 +48,8 @@ QT_MOC_LITERAL(12, 384, 40), // "on_pushButton_StartSine_2sign..."
 QT_MOC_LITERAL(13, 425, 40), // "on_pushButton_StartSine_3sign..."
 QT_MOC_LITERAL(14, 466, 40), // "on_pushButton_StartSine_4sign..."
 QT_MOC_LITERAL(15, 507, 26), // "on_pushButton_Stop_clicked"
-QT_MOC_LITERAL(16, 534, 48) // "on_pushButton_SimulateParamet..."
+QT_MOC_LITERAL(16, 534, 48), // "on_pushButton_SimulateParamet..."
+QT_MOC_LITERAL(17, 583, 27) // "on_pushButton_Close_clicked"
 
     },
     "MainWindow\0serialDataReceived\0\0"
@@ -65,7 +66,8 @@ QT_MOC_LITERAL(16, 534, 48) // "on_pushButton_SimulateParamet..."
     "on_pushButton_StartSine_3signals_clicked\0"
     "on_pushButton_StartSine_4signals_clicked\0"
     "on_pushButton_Stop_clicked\0"
-    "on_pushButton_SimulateParametersSequence_clicked"
+    "on_pushButton_SimulateParametersSequence_clicked\0"
+    "on_pushButton_Close_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -75,7 +77,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      15,   14, // methods
+      16,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -83,23 +85,25 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   89,    2, 0x08 /* Private */,
-       3,    0,   90,    2, 0x08 /* Private */,
-       4,    0,   91,    2, 0x08 /* Private */,
-       5,    0,   92,    2, 0x08 /* Private */,
-       6,    0,   93,    2, 0x08 /* Private */,
-       7,    0,   94,    2, 0x08 /* Private */,
-       8,    0,   95,    2, 0x08 /* Private */,
-       9,    0,   96,    2, 0x08 /* Private */,
-      10,    0,   97,    2, 0x08 /* Private */,
-      11,    0,   98,    2, 0x08 /* Private */,
-      12,    0,   99,    2, 0x08 /* Private */,
-      13,    0,  100,    2, 0x08 /* Private */,
-      14,    0,  101,    2, 0x08 /* Private */,
-      15,    0,  102,    2, 0x08 /* Private */,
-      16,    0,  103,    2, 0x08 /* Private */,
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    0,   99,    2, 0x08 /* Private */,
+       8,    0,  100,    2, 0x08 /* Private */,
+       9,    0,  101,    2, 0x08 /* Private */,
+      10,    0,  102,    2, 0x08 /* Private */,
+      11,    0,  103,    2, 0x08 /* Private */,
+      12,    0,  104,    2, 0x08 /* Private */,
+      13,    0,  105,    2, 0x08 /* Private */,
+      14,    0,  106,    2, 0x08 /* Private */,
+      15,    0,  107,    2, 0x08 /* Private */,
+      16,    0,  108,    2, 0x08 /* Private */,
+      17,    0,  109,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -140,6 +144,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 12: _t->on_pushButton_StartSine_4signals_clicked(); break;
         case 13: _t->on_pushButton_Stop_clicked(); break;
         case 14: _t->on_pushButton_SimulateParametersSequence_clicked(); break;
+        case 15: _t->on_pushButton_Close_clicked(); break;
         default: ;
         }
     }
@@ -175,13 +180,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 15)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 15;
+        _id -= 16;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 15)
+        if (_id < 16)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 15;
+        _id -= 16;
     }
     return _id;
 }
